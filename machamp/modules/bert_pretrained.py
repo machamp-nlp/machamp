0e6e4886d69ad23cbc64d6d5b7304441b61bbe43
robvanderg <robvanderg@live.nl>
2020-07-03 14:06:18 +0000

    Removed superfluous print statements

--- machamp/modules/bert_pretrained.py
+++ machamp/modules/bert_pretrained.py
@@ -271,7 +271,6 @@ class WordpieceIndexer(TokenIndexer[int]):
         # to correspond to the remaining tokens after truncation, which
         # is captured by the offsets.
         mask = [1 for _ in offsets]
-        print("somewhere", token_type_ids)
         return {
             index_name: wordpiece_ids,
             f"{index_name}-offsets": offsets,
@@ -397,7 +396,6 @@ def _get_token_type_ids(wordpiece_ids: List[int],
         else:
             cursor += 1
             token_type_ids.append(type_id)
-    print('get', token_type_ids)
     return token_type_ids
 
 
@@ -481,8 +479,6 @@ class BertEmbedder(TokenEmbedder):
             the second sentence should have type 1.  If you don't provide this
             (the default BertIndexer doesn't) then it's assumed to be all 0s.
         """
-        print('forward', token_type_ids)
-        exit(1)
         # pylint: disable=arguments-differ
         batch_size, full_seq_len = input_ids.size(0), input_ids.size(-1)
         initial_dims = list(input_ids.shape[:-1])
@@ -525,17 +521,7 @@ class BertEmbedder(TokenEmbedder):
         # input_ids may have extra dimensions, so we reshape down to 2-d
         # before calling the BERT model and then reshape back at the end.
         token_type_ids = torch.zeros_like(input_ids)
-        print(input_ids.shape)
-        print(token_type_ids.shape)
-        print(input_mask.shape)
 
-        print(input_ids)
-        print(token_type_ids)
-        print(input_mask)
-
-        print(util.combine_initial_dims(input_ids).shape)
-        print(util.combine_initial_dims(token_type_ids).shape)
-        print(util.combine_initial_dims(input_mask).shape)
         all_encoder_layers, _ = self.bert_model(input_ids=util.combine_initial_dims(input_ids),
                                                 token_type_ids=util.combine_initial_dims(token_type_ids),
                                                 attention_mask=util.combine_initial_dims(input_mask))
