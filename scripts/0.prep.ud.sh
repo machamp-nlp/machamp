a942ed9f28a649c1a90c1e0e522126420ccec4e8
robvanderg <robvanderg@live.nl>
2022-10-07 07:21:22 +0200

    Fixed 0.prep.ud.sh

--- scripts/0.prep.ud.sh
+++ scripts/0.prep.ud.sh
@@ -1,8 +1,11 @@
+#wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-2837/ud-treebanks-v2.2.tgz
 #wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3424/ud-treebanks-v2.7.tgz
 #wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3687/ud-treebanks-v2.8.tgz
-wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-4611/ud-treebanks-v2.9.tgz
-tar -zxvf ud-treebanks-v2.9.tgz
-mv ud-treebanks-v2.9 data
-cp -r data/ud-treebanks-v2.9 data/ud-treebanks-v2.9.singleToken
-python3 scripts/misc/cleanconl.py data/ud-treebanks-v2.9.singleToken/*/*conllu
+#wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-4611/ud-treebanks-v2.9.tgz
+mkdir -p data
+wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-4758/ud-treebanks-v2.10.tgz
+tar -zxvf ud-treebanks-v2.10.tgz
+mv ud-treebanks-v2.10 data
+cp -r data/ud-treebanks-v2.10 data/ud-treebanks-v2.10.singleToken
+python3 scripts/misc/cleanconl.py data/ud-treebanks-v2.10.singleToken/*/*conllu
 
