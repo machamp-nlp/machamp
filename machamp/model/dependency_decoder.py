e162ab52c92deaca5e405cfb982fa79b8c1825d8
robvanderg <robvanderg@live.nl>
2023-11-16 14:31:36 +0100

    Replaced  torch.cuda.LongTensor as it will be deprecated

--- machamp/model/dependency_decoder.py
+++ machamp/model/dependency_decoder.py
@@ -22,7 +22,7 @@ def get_range_vector(size: int, device: int) -> torch.Tensor:
     is meant to avoid copy data from CPU to GPU.
     """
     if device > -1:
-        return torch.cuda.LongTensor(size, device=device).fill_(1).cumsum(0) - 1
+        return torch.ones(size, device=device, dtype=torch.long).cumsum(0) - 1
     else:
         return torch.arange(0, size, dtype=torch.long)
 
